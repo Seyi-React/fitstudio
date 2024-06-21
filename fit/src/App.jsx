10a164f2b18b185762481ae1bd628ef9724b86ca
seyi-React <oluwaseyiadeoti825@gmail.com>
2024-06-21 17:21:38 +0100

    login onbaord done

--- fit/src/App.jsx
+++ fit/src/App.jsx
@@ -1,18 +1,39 @@
-import { useState } from "react";
-// import CreateAcoount from "./components/onboard/CreateAcoount";
+import { useEffect, useState } from "react";
+import { Routes,Route,useLocation,useNavigate} from "react-router-dom"
+import Login from "./components/onboard/Login/Login"
 
 
 import "./App.css";
 import CreateAcoount from "./components/onboard/CreateAcoount";
+import Loading from "./components/LoadingSpin/Loading";
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 2 seconds
+
+    return () => clearTimeout(timer);
+  }, [location]);
  
 
   return (
     <>
-      <div>
-        <CreateAcoount />
-      </div>
+      {/* <BrowserRouter> */}
+      {loading && <Loading />}
+        <Routes>
+        
+          <Route path="/" Component={CreateAcoount} />
+          <Route path="/login" Component={Login} />
+        </Routes>
+      
+      {/* </BrowserRouter> */}
     </>
   );
 }
--- fit/src/components/onboard/Login/Login.jsx
+++ fit/src/components/onboard/Login/Login.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import style from "./createaccount.module.css";
-import google from "../../assets/Google.png";
-import { Link } from "react-router-dom";
+import React from 'react'
+import style from "./Login.module.css"
+import google from "../../../assets/Google.png"
+import {Link } from "react-router-dom"
 
-const CreateAcoount = () => {
+const Login = () => {
   return (
-    <div style={{ background: "" }}>
+    <div>
+       <div style={{ background: "" }}>
       <div className={style.size}>
         <div className={style.account_div}>
           <div>
-            <h3>Create an account</h3>
-            <p>Enter your email to sign up for this app</p>
+            <h3>Login</h3>
+            <p>Enter your details to login</p>
             <div className={style.input}>
               <input
                 type="email"
@@ -24,12 +25,12 @@ const CreateAcoount = () => {
                 id="password"
                 placeholder="Create Password"
               />
-              <button className={style.continue}>Continue</button>
+              <button className={style.continue}>Login</button>
             </div>
           </div>
           <div className={style.or}>
             <div className={style.dash}></div>
-            <small>or</small>
+            <small>or login</small>
             <div className={style.dash}></div>
           </div>
           <div>
@@ -38,13 +39,14 @@ const CreateAcoount = () => {
               <small>Google</small>
             </div>
             <small className={style.account}>
-              Already have an account? <Link to="/login" className={style.link}>Login</Link>
+              Don't have an account? <Link to="/" className={style.link}>Sign Up</Link>
             </small>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+    </div>
+  )
+}
 
-export default CreateAcoount;
+export default Login
